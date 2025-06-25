96812385949aa96a9d9a078a3d73ad3f78a7a755
Wataane <ansh.watane2003@gmail.com>
2025-06-25 09:37:23 +0530

    Delete document-service directory

--- document-service/src/main/java/com/collabdoc/repository/DocumentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.collabdoc.repository;
-
-import com.collabdoc.model.Document;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface DocumentRepository extends JpaRepository<Document, Long> {
-    List<Document> findByOwnerEmail(String ownerEmail);
-}
